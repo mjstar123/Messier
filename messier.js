2e87e83423a657ebd67597b2f39674058b21d5e6
myeongjun Kim <starmintaka0616@gmail.com>
2025-06-12 10:23:58 +0900

    third

--- messier.js
+++ messier.js
@@ -1,6 +1,15 @@
 const section = document.getElementById('messier-section');
 const asides = document.getElementById('messier-asides');
 
+const messierNames = [
+    "게 성운", "구상성단 M2", "구상성단 M3", "구상성단 M4", "구상성단 M5",
+    "나비 성단", "Ptolemy 성단", "라군 성운", "구상성단 M9", "구상성단 M10",
+    "구상성단 M11", "구상성단 M12", "구상성단 M13", "구상성단 M14", "구상성단 M15",
+    "구상성단 M16", "독수리 성운", "오메가 성운", "구상성단 M19", "구상성단 M20",
+    "삼열 성운", "구상성단 M22", "구상성단 M23", "구상성단 M24", "구상성단 M25",
+    "구상성단 M26", "구상성단 M27", "덤벨 성운", "구상성단 M29", "구상성단 M30"
+  ];
+
 for (let i = 1; i <= 30; i++) {
     const num = i.toString().padStart(2, '0');
 
@@ -20,7 +29,6 @@ for (let i = 1; i <= 30; i++) {
     `;
     section.appendChild(article);
 
-    
     const aside = document.createElement('aside');
     aside.classList.add('messier-aside');
 
@@ -28,6 +36,7 @@ for (let i = 1; i <= 30; i++) {
         <div class="txt">
             <h1>${num}</h1>
             <p>메시에 목록 ${i}번 설명입니다.</p>
+            <p>${messierNames[i-1]}</p>
             <span class="close">CLOSE</span>  
         </div>
         <figure>
